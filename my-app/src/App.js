7974367ca25ef9c3e22ab10e1f298ace126617c1
Daniel <daniel.selistean@yahoo.com>
2020-05-26 07:05:25 +0300

    Added delete button and delete functionality

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -19,11 +19,25 @@ class App extends Component {
         })
       })
   }
+
+  deleteGame = (_id) => {
+    fetch('https://games-app-siit.herokuapp.com/games/' + _id, { method: 'DELETE' })
+      .then(resp => resp.text()).then(apiresponse => console.log(apiresponse))
+      
+    let games = this.state.games.filter(game => {
+      return game._id !== _id
+    })
+    this.setState({
+      isLoaded: true,
+      games: games
+    })
+  }
+
   render() {
 
     return (
       <div className="App" >
-        <Game games={this.state.games} />
+        <Game games={this.state.games} deleteGame={this.deleteGame} />
       </div>
     );
   }
--- my-app/src/Game.js
+++ my-app/src/Game.js
@@ -2,14 +2,17 @@ import React from 'react';
 
 function Game(props) {
 
-    const { games } = props;
-    const listGames = games.map(game => (
-        <div className="game" key={game._id}>
-            <h1>{game.title}</h1>
-            <p>{game.description}</p>
-            <img src={game.imageUrl} alt="game" />
-        </div>
-    ));
+    const { games,deleteGame } = props;
+    const listGames = games.map(game => {
+        return (
+            <div className="game" key={game._id}>
+                <h1>{game.title}</h1>
+                <p>{game.description}</p>
+                <img src={game.imageUrl} alt="game" />
+                <button onClick={() => {deleteGame(game._id)}}>Delete Game</button>
+            </div>
+        )
+    });
     return (
         <div className="list-games">
             {listGames}
